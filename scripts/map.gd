6577650b7e301d30a857a59ef40dcfe40cf76c85
Benjamin Baer <cudgeon@gmail.com>
2026-08-06 12:54:07 +0200

    base game setup and folder structure

---
+++ scripts/map.gd
@@ -0,0 +1,34 @@
+extends TileMapLayer
+
+const TILE_FLOOR : Vector2i = Vector2i(1,0)
+const TILE_CHEST : Vector2i = Vector2i(3,0)
+const POS_CHEST : Vector2i = Vector2i(8,8)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	placeTreasure(POS_CHEST)
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton and event.pressed:
+		digIntoWall(globalToGridPos(get_local_mouse_position()))
+
+func globalToGridPos(globalPos : Vector2) -> Vector2i:
+	var x = globalPos.x / tile_set.tile_size.x
+	var y = globalPos.y / tile_set.tile_size.y
+	return Vector2i(x,y)
+
+func gridToGlobalPos(gridPos : Vector2i) -> Vector2:
+	var x = gridPos.x * tile_set.tile_size.x
+	var y = gridPos.y * tile_set.tile_size.y
+	return Vector2(x,y)
+	
+func isWall(gridPos : Vector2i) -> bool:
+	return get_cell_tile_data(gridPos).get_custom_data("wall")
+
+func digIntoWall(gridPos:Vector2i) -> void:
+	if isWall(gridPos):
+		set_cell(gridPos, 0, TILE_FLOOR)
+		
+func placeTreasure(gridPos : Vector2i) -> void:
+	set_cell(gridPos,0,TILE_CHEST)
---
+++ scripts/pathfinder.gd
@@ -0,0 +1,70 @@
+class_name Pathfinder
+extends Node
+
+
+var astar_grid
+var storages_in_world = []
+
+@export var tile_map : TileMapLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = tile_map.get_used_rect()
+	astar_grid.cell_size = Vector2(16,16)
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_AT_LEAST_ONE_WALKABLE
+	astar_grid.update()
+	
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tile_map.get_used_rect().position.x
+				,y + tile_map.get_used_rect().position.y )
+			var ground_data = tile_map.ground.get_cell_tile_data(tile_position)
+			
+			if ground_data != null and ground_data.get_custom_data("solid"):
+				astar_grid.set_point_solid(tile_position)
+
+func get_my_path(start, to):
+	var id_path = astar_grid.get_id_path(
+		tile_map.local_to_map(start), 
+		tile_map.local_to_map(to),
+		true
+		).slice(1)
+	return id_path
+
+func get_my_points(start, to):
+	var point_path = astar_grid.get_point_path(
+		tile_map.local_to_map(start), 
+		tile_map.local_to_map(to),
+		true
+		).slice(1)
+	return point_path
+
+func move_solid(old_position, new_position):
+	if(old_position):
+		astar_grid.set_point_solid(tile_map.local_to_map(old_position), false)
+	astar_grid.set_point_solid(tile_map.local_to_map(new_position))
+
+func find_nearest_storage(my_position: Vector2):
+	var closestStorage : Vector2
+	var distance = 9999999	
+	
+	
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tile_map.get_used_rect().position.x
+				,y + tile_map.get_used_rect().position.y )
+			var structure_data = tile_map.structure.get_cell_tile_data(tile_position)
+			if structure_data != null:
+				if structure_data.terrain_set == 2:
+					storages_in_world.append(tile_position)
+	if !storages_in_world.is_empty():
+		for storage in storages_in_world:
+			storage = tile_map.gridToGlobalPos(storage)
+			var distance_new = get_my_path(my_position, storage).size()
+			if(distance_new < distance):
+				distance = distance_new
+			closestStorage = storage
+	return closestStorage
